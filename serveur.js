d5b99a0d76e416cf542a3b7ee39d63eceabd896f
RodolpheT <rodolphe.toyer@powerreviews.com>
2016-05-16 23:44:27 -0500

    Je pense pouvoir gérer les requetes à la gopro et la communication serial avec le teensy directement dans le serveur node

--- serveur.js
+++ serveur.js
@@ -2,7 +2,64 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
-var rootFolder = 'E:/Mes documents/Boulot/Workspace/localhost/Rodolphe';
+var http = require('http');
+var serialport = require('serialport');       // include the serialport library
+var SERVER_PORT = 8081;
+
+// configure the serial port:
+SerialPort = serialport.SerialPort,             // make a local instance of serialport
+    portName = process.argv[2];					// get serial port name from the command line
+
+var serialOptions = {                           // serial communication options
+      baudRate: 9600,                           // data rate: 9600 bits per second
+      parser: serialport.parsers.readline('\n') // newline generates a data event
+    };
+
+// If the user didn't give a serial port name, exit the program:
+if (typeof portName === "undefined") {
+  console.log("You need to specify the serial port when you launch this script, like so:\n");
+  console.log("    node server.js <portname>");
+  console.log("\n Fill in the name of your serial port in place of <portname> \n");
+  process.exit(1);
+}
+// open the serial port:
+var myPort = new SerialPort(portName, serialOptions);
+
+// set up event listeners for the serial events:
+myPort.on('open', showPortOpen);
+myPort.on('data', takePicture);
+myPort.on('close', showPortClose);
+myPort.on('error', showError);
+
+// ------------------------ Serial event functions:
+// this is called when the serial port is opened:
+function showPortOpen() {
+  console.log('port open. Data rate: ' + myPort.options.baudRate);
+}
+
+var options = {
+	  host: 'http://10.5.5.9',
+	  path: '/camera/SH?t=G2ST4G22&p=%01'
+	};
+	
+function takePicture() {
+	console.log('Sending GoPro picture request...');
+	var req = http.get(options, function(res) {
+	  console.log('STATUS: ' + res.statusCode);
+	});
+
+	req.on('error', function(e) {
+	  console.log('ERROR: ' + e.message);
+	});
+}
+
+function showPortClose() {
+   console.log('port closed.');
+}
+// this is called when the serial port has an error:
+function showError(error) {
+  console.log('Serial port error: ' + error);
+}
 
 var server = http.createServer(function(req, res) {
   
@@ -57,4 +114,4 @@ var server = http.createServer(function(req, res) {
 		}
 	}
 });
-server.listen(8081);
+server.listen(SERVER_PORT);
