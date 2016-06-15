e1143f589e9b6d35806178ed59325aafbd5e6d87
RodolpheT <rodolphe.toyer@powerreviews.com>
2016-06-14 21:06:16 -0500

    multitasking

--- teensy_code/Photobooth/Photobooth/Photobooth.ino
+++ teensy_code/Photobooth/Photobooth/Photobooth.ino
@@ -4,227 +4,357 @@
   #include <avr/power.h>
 #endif
 
-#define PIN_LEDRING 17
+#define PIN_LEDRINGFLASH 17
 #define PIN_BUTTON 2
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN_LEDRING, NEO_RGBW + NEO_KHZ800);
+// Pattern types supported:
+enum  pattern { NONE, RAINBOW_CYCLE, THEATER_CHASE, COLOR_WIPE, SCANNER, FADE };
+// Patern directions supported:
+enum  direction { FORWARD, REVERSE };
+ 
+// NeoPattern Class - derived from the Adafruit_NeoPixel class
+class NeoPatterns : public Adafruit_NeoPixel
+{
+    public:
+ 
+    // Member Variables:  
+    pattern  ActivePattern;  // which pattern is running
+    direction Direction;     // direction to run the pattern
+    
+    unsigned long Interval;   // milliseconds between updates
+    unsigned long lastUpdate; // last update of position
+    
+    uint32_t Color1, Color2;  // What colors are in use
+    uint16_t TotalSteps;  // total number of steps in the pattern
+    uint16_t Index;  // current step within the pattern
+    
+    void (*OnComplete)();  // Callback on completion of pattern
+    
+    // Constructor - calls base-class constructor to initialize strip
+    NeoPatterns(uint16_t pixels, uint8_t pin, uint8_t type, void (*callback)())
+    :Adafruit_NeoPixel(pixels, pin, type)
+    {
+        OnComplete = callback;
+    }
+    
+    // Update the pattern
+    void Update()
+    {
+        if((millis() - lastUpdate) > Interval) // time to update
+        {
+            lastUpdate = millis();
+            switch(ActivePattern)
+            {
+                case RAINBOW_CYCLE:
+                    RainbowCycleUpdate();
+                    break;
+                case THEATER_CHASE:
+                    TheaterChaseUpdate();
+                    break;
+                case COLOR_WIPE:
+                    ColorWipeUpdate();
+                    break;
+                case SCANNER:
+                    ScannerUpdate();
+                    break;
+                case FADE:
+                    FadeUpdate();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+  
+    // Increment the Index and reset at the end
+    void Increment()
+    {
+        if (Direction == FORWARD)
+        {
+           Index++;
+           if (Index >= TotalSteps)
+            {
+                Index = 0;
+                if (OnComplete != NULL)
+                {
+                    OnComplete(); // call the comlpetion callback
+                }
+            }
+        }
+        else // Direction == REVERSE
+        {
+            --Index;
+            if (Index <= 0)
+            {
+                Index = TotalSteps-1;
+                if (OnComplete != NULL)
+                {
+                    OnComplete(); // call the comlpetion callback
+                }
+            }
+        }
+    }
+    
+    // Reverse pattern direction
+    void Reverse()
+    {
+        if (Direction == FORWARD)
+        {
+            Direction = REVERSE;
+            Index = TotalSteps-1;
+        }
+        else
+        {
+            Direction = FORWARD;
+            Index = 0;
+        }
+    }
+    
+    // Initialize for a RainbowCycle
+    void RainbowCycle(uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = RAINBOW_CYCLE;
+        Interval = interval;
+        TotalSteps = 255;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Rainbow Cycle Pattern
+    void RainbowCycleUpdate()
+    {
+        for(int i=0; i< numPixels(); i++)
+        {
+            setPixelColor(i, Wheel(((i * 256 / numPixels()) + Index) & 255));
+        }
+        show();
+        Increment();
+    }
+ 
+    // Initialize for a Theater Chase
+    void TheaterChase(uint32_t color1, uint32_t color2, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = THEATER_CHASE;
+        Interval = interval;
+        TotalSteps = numPixels();
+        Color1 = color1;
+        Color2 = color2;
+        Index = 0;
+        Direction = dir;
+   }
+    
+    // Update the Theater Chase Pattern
+    void TheaterChaseUpdate()
+    {
+        for(int i=0; i< numPixels(); i++)
+        {
+            if ((i + Index) % 3 == 0)
+            {
+                setPixelColor(i, Color1);
+            }
+            else
+            {
+                setPixelColor(i, Color2);
+            }
+        }
+        show();
+        Increment();
+    }
+ 
+    // Initialize for a ColorWipe
+    void ColorWipe(uint32_t color, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = COLOR_WIPE;
+        Interval = interval;
+        TotalSteps = numPixels();
+        Color1 = color;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Color Wipe Pattern
+    void ColorWipeUpdate()
+    {
+        setPixelColor(Index, Color1);
+        show();
+        Increment();
+    }
+    
+    // Initialize for a SCANNNER
+    void Scanner(uint32_t color1, uint8_t interval)
+    {
+        ActivePattern = SCANNER;
+        Interval = interval;
+        TotalSteps = (numPixels() - 1) * 2;
+        Color1 = color1;
+        Index = 0;
+    }
+ 
+    // Update the Scanner Pattern
+    void ScannerUpdate()
+    { 
+        for (int i = 0; i < numPixels(); i++)
+        {
+            if (i == Index)  // Scan Pixel to the right
+            {
+                 setPixelColor(i, Color1);
+            }
+            else if (i == TotalSteps - Index) // Scan Pixel to the left
+            {
+                 setPixelColor(i, Color1);
+            }
+            else // Fading tail
+            {
+                 setPixelColor(i, DimColor(getPixelColor(i)));
+            }
+        }
+        show();
+        Increment();
+    }
+    
+    // Initialize for a Fade
+    void Fade(uint32_t color1, uint32_t color2, uint16_t steps, uint8_t interval, direction dir = FORWARD)
+    {
+        ActivePattern = FADE;
+        Interval = interval;
+        TotalSteps = steps;
+        Color1 = color1;
+        Color2 = color2;
+        Index = 0;
+        Direction = dir;
+    }
+    
+    // Update the Fade Pattern
+    void FadeUpdate()
+    {
+        // Calculate linear interpolation between Color1 and Color2
+        // Optimise order of operations to minimize truncation error
+        uint8_t red = ((Red(Color1) * (TotalSteps - Index)) + (Red(Color2) * Index)) / TotalSteps;
+        uint8_t green = ((Green(Color1) * (TotalSteps - Index)) + (Green(Color2) * Index)) / TotalSteps;
+        uint8_t blue = ((Blue(Color1) * (TotalSteps - Index)) + (Blue(Color2) * Index)) / TotalSteps;
+        
+        ColorSet(Color(red, green, blue));
+        show();
+        Increment();
+    }
+   
+    // Calculate 50% dimmed version of a color (used by ScannerUpdate)
+    uint32_t DimColor(uint32_t color)
+    {
+        // Shift R, G and B components one bit to the right
+        uint32_t dimColor = Color(Red(color) >> 1, Green(color) >> 1, Blue(color) >> 1);
+        return dimColor;
+    }
+ 
+    // Set all pixels to a color (synchronously)
+    void ColorSet(uint32_t color)
+    {
+        for (int i = 0; i < numPixels(); i++)
+        {
+            setPixelColor(i, color);
+        }
+        show();
+    }
+ 
+    // Returns the Red component of a 32-bit color
+    uint8_t Red(uint32_t color)
+    {
+        return (color >> 16) & 0xFF;
+    }
+ 
+    // Returns the Green component of a 32-bit color
+    uint8_t Green(uint32_t color)
+    {
+        return (color >> 8) & 0xFF;
+    }
+ 
+    // Returns the Blue component of a 32-bit color
+    uint8_t Blue(uint32_t color)
+    {
+        return color & 0xFF;
+    }
+    
+    // Input a value 0 to 255 to get a color value.
+    // The colours are a transition r - g - b - back to r.
+    uint32_t Wheel(byte WheelPos)
+    {
+        WheelPos = 255 - WheelPos;
+        if(WheelPos < 85)
+        {
+            return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+        }
+        else if(WheelPos < 170)
+        {
+            WheelPos -= 85;
+            return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+        }
+        else
+        {
+            WheelPos -= 170;
+            return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+        }
+    }
+};
+
+ 
+// Define some NeoPatterns for the two rings and the stick
+//  as well as some completion routines
+NeoPatterns Ring1(60, PIN_LEDRINGFLASH, NEO_RGBW + NEO_KHZ800,NULL);
+
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN_LEDRINGFLASH, NEO_RGBW + NEO_KHZ800);
 int buttonState = 0; 
 
 void setup() {
+  //initialize pin 13 to use the built in LED
   pinMode(13, OUTPUT);
-  pinMode(PIN_BUTTON, INPUT_PULLUP);
-  Serial.begin(9600);
-  
-  strip.begin();
-  strip.setBrightness(10);
-  strip.show(); // Initialize all pixels to 'off'
-
   digitalWrite(13, HIGH); //lighting up the built-in led to give proof of life
+  
+  //initialize the main blue button to take picture
+  pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+  //initialize serial communication with computer
+  Serial.begin(9600);
+
+  // Initialize all the pixelStrips
+  Ring1.begin();
+  Ring1.RainbowCycle(10);
+  
 }
 
 void loop() {
-  strip.setBrightness(15);
+
+  // Update the rings.
+  Ring1.Update();
+  
   if (!digitalRead(PIN_BUTTON)) {
     delay(50);
     if(!digitalRead(PIN_BUTTON)){
-        buttonPressedSequence();  //calling the light sequence + serial request for picture
+        // Switch Ring1 to FASE pattern
+        Ring1.ActivePattern = COLOR_WIPE;
+        Ring1.Color(255,0,0)
+        Ring1.Interval = 20;
     }
   }
-   fullColor(strip.Color(0,0,0));
    
-  //rainbowMiddle(50);
- 
 }
 
 void buttonPressedSequence(){
-  fullColor(strip.Color(0,0,0));
-  delay(200);
-  fullColor(strip.Color(255,0,0));
-  delay(200);
-  fullColor(strip.Color(0,0,0));
-  delay(200);
-  fullColor(strip.Color(255,0,0));
-  delay(200);
-  fullColor(strip.Color(0,0,0));
-  delay(200);
-  fullColor(strip.Color(255,0,0));
-  colorWipe(strip.Color(0,0, 0), 70); // Green
-  //strip.setBrightness(200);
-  fullColor(strip.Color(255,255,255));
-  delay(100);
-  Serial.println("PictureRequest"); 
+//  fullColor(strip.Color(0,0,0));
+//  delay(200);
+//  fullColor(strip.Color(255,0,0));
+//  delay(200);
+//  fullColor(strip.Color(0,0,0));
+//  delay(200);
+//  fullColor(strip.Color(255,0,0));
+//  delay(200);
+//  fullColor(strip.Color(0,0,0));
+//  delay(200);
+//  fullColor(strip.Color(255,0,0));
+//  colorWipe(strip.Color(0,0, 0), 70); // Green
+//  //strip.setBrightness(200);
+//  fullColor(strip.Color(255,255,255));
+//  delay(100);
+//  Serial.println("PictureRequest"); 
   delay(2000);
 }
 
-void fullColor(uint32_t c){
-  for(uint16_t i=0;i<strip.numPixels();i++){
-      strip.setPixelColor(i,c);
-      }
-  strip.show();
-}
-
-// Empty all strip
-void resetStrip(){
-  for(uint16_t i=0;i<strip.numPixels();i++){
-      strip.setPixelColor(i,0,0,0);
-      }
-  strip.show();
-}
- 
-// Fill the dots one after the other with a color and reset the trail to create a n length snake
-void colorSnake(uint32_t c, uint8_t snakeLength, uint8_t wait) {
-  resetStrip();
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-
-      if(i>snakeLength){
-            strip.setPixelColor(i-snakeLength, 0, 0, 0);
-      }
-
-      strip.show();
-      delay(wait);
-  }
-}    
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-//Create a blinker effect in the direction required (1= right, 0=left)
-void blinkerMode(uint8_t blinkerDirection){
-uint8_t i, j,k;
-
-for(k=0;k<3;k++){
-    resetStrip();
-    for(i=25;i<37;i++){
-      strip.setPixelColor(60-i, 250,173-2*i,22);
-      strip.show();
-      delay(55);
-    }
-    
-    for(j=0;j<4;j++){
-      resetStrip();
-      delay(200);
-      for(i=23;i>0;i--){
-        strip.setPixelColor(i,255,0,0);
-      }
-      strip.show();
-      delay(200);
-    }
-}
-}
-
-// Fill the dots one after the other with a color
-void colorWipeReverse(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(strip.numPixels()-i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-// Fill the dots one after the other with a color
-void colorWipeMiddle(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels()/2; i++) {
-      strip.setPixelColor(i+strip.numPixels()/2, c);
-	  strip.setPixelColor(strip.numPixels()/2-i, c);
-      strip.show();
-      delay(wait);
-  }
-}
-
-// Fill the dots one after the other with a color
-void rainbowMiddle(uint8_t wait) {
-
-uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels()/2; i++) {
-      strip.setPixelColor(i+strip.numPixels()/2, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-	  strip.setPixelColor(strip.numPixels()/2-i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}
