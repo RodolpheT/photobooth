9af55ebbb26fb67fe47204bf4250cf40ad9ae770
RodolpheT <rodolphe.toyer@powerreviews.com>
2016-06-16 08:55:50 -0500

    Decreased wait between checks

--- python/downloadImages.py
+++ python/downloadImages.py
@@ -4,13 +4,14 @@ import time
 from shutil import copyfile
 import os
 
+print "Photobooth - Download GoPro images script. Started..."
+    
 while True:
-    time.sleep(0.5)
+    time.sleep(0.1)
     response = urllib2.urlopen('http://10.5.5.9:8080/videos/DCIM/102GOPRO/')
     html = response.read()
     soup = BeautifulSoup(html)
     dirlist = os.listdir('C:/Users/rtoyer.LAP-PAR-RTOYER2/Desktop/photobooth/photobooth/pictures')
-    print "Check..."
     for lowResImage in soup.findAll('span','size'):
         if lowResImage.getText() == "1":
             newImage = lowResImage.parent.parent.find('a').get('href')
